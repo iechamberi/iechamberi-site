0d33f285ddefed71df00f561f2cdf9da8b16553b
Abel N. Nieva <931497+abelnnieva@users.noreply.github.com>
2021-09-24 21:27:47 +0200

    feat: new our-history page was created

---
+++ pages/our-history.vue
@@ -0,0 +1,57 @@
+<template>
+  <CBox>
+    <PageHeader :title="page.header.title" :summary="page.header.summary" />
+    <DynamicZone :sections="page.sections" />
+  </CBox>
+</template>
+
+<script>
+import PAGE_BY_SLUG_QUERY from '@/apollo/queries/pages/page-by-slug'
+import getMetaTags from '@/utils/meta-tags'
+import { last } from 'lodash'
+
+export default {
+  nuxtI18n: {
+    paths: {
+      es: '/nuestra-historia',
+      en: '/our-history',
+    },
+  },
+  async asyncData({ app, i18n }) {
+    const client = app.apolloProvider.defaultClient
+    const slug = last(app.localePath('our-history').split('/'))
+    const { data } = await client.query({
+      query: PAGE_BY_SLUG_QUERY,
+      variables: { slug, locale: i18n.locale },
+    })
+    return {
+      page: data.pageBySlug,
+    }
+  },
+  head() {
+    const i18nHead = this.$nuxtI18nHead({ addSeoAttributes: true })
+    const metaTags = getMetaTags(this.page.metaTags)
+    return {
+      title: this.page.metaTags.title,
+      htmlAttrs: {
+        ...i18nHead.htmlAttrs,
+      },
+      meta: [
+        {
+          hid: 'og:url',
+          property: 'og:url',
+          content: this.canonicalUrl,
+        },
+        ...metaTags,
+        ...i18nHead.meta,
+      ],
+      link: [...i18nHead.link],
+    }
+  },
+  computed: {
+    canonicalUrl() {
+      return `${this.$config.baseUrl}${this.localePath('our-history')}`
+    },
+  },
+}
+</script>
